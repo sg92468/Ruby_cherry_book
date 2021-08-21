7ce4cea73c31fa05ef9c138d23c778008e1180bf
sg92468 <sg92468@gmail.com>
2021-08-21 21:26:24 +0900

    例題5.5

---
+++ test/chapter5.rb
@@ -0,0 +1,9 @@
+currentries = { 'japan' => 'yen', 'us' => 'dollar', 'india' => 'rupee' }
+currentries['italy'] = 'euro'
+
+currentries.each do |key_value|
+  key = key_value[0]
+  value = key_value[1]
+  puts "#{key} : #{value}"
+  puts "#{key_value}"
+end
