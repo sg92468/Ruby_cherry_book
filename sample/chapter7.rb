a025844cc21c1181038b098b10ff0a0d84cc1aca
sg92468 <sg92468@gmail.com>
2021-08-28 19:00:30 +0900

    7.10.7

--- sample/chapter7.rb
+++ sample/chapter7.rb
@@ -6,3 +6,24 @@ user = User.new
 p user.nil?
 p User.superclass
 p user.class
+
+class User
+  def initialize(name)
+    @name = name
+  end
+
+  def hello
+    "Hello, #{@name}!"
+  end
+end
+
+class User
+  alias hello_original hello
+
+  def hello
+    "#{hello_original}じゃなくて、#{@name}さん、こんにちは！"
+  end
+end
+
+user = User.new('Alice')
+p user.hello
