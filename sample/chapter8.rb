f1916e6508c201181f4880039fee95af382614f5
sg92468 <sg92468@gmail.com>
2021-08-31 21:13:02 +0900

    8.6

---
+++ sample/chapter8.rb
@@ -0,0 +1,17 @@
+class Second
+  def initialize(player, uniform_number)
+    @player = player
+    @uniform_number = uniform_number
+  end
+end
+
+module Clock
+  class Second
+    def initialize(digits)
+      @digits = digits
+      @baseball_second = ::Second.new('Clock', 10)
+    end
+  end
+end
+
+p Clock::Second.new(13)
